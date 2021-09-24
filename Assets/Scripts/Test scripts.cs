3b7bb87662d1f8480af534cb33489d017fc8a8af
Olav RK <OlavRKvam@live.com>
2021-09-24 13:40:24 +0200

    test 1

--- Assets/Scripts/Test scripts.cs
+++ Assets/Scripts/Test scripts.cs
@@ -7,7 +7,7 @@ public class Testscripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ijdsgfsgsghsouhgosngshngsihnsih
     }
 
     // Update is called once per frame
