34985673fbdfe55ae7e8f184c648d2e06e8bcfcb
Gardathor <gard.kvilvang@gmail.com>
2021-09-30 10:05:47 +0200

    start torsdag

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -8,6 +8,8 @@ public class PlayerAnimations : MonoBehaviour
     private PlayerInput input;
     private TestPlayerMovement movement; // this needs to be changed when final product
     private Rigidbody2D rb2d;
+
+    private string action;
     
     
     void Start()
@@ -21,39 +23,53 @@ public class PlayerAnimations : MonoBehaviour
     
     void Update()
     {
-
+        
         if (input.moveVector.x != 0)
         {
             transform.localScale = new Vector2(input.moveVector.x, 1f); // this will flip the player sprite to have it face the direction it is running
         }
-        
-        if (movement.IsGrounded())
+
+        if (action != "attack")
         {
-            if (input.moveVector.x != 0)
+            if (movement.IsGrounded())
             {
-                animator.Play("Run");
+                if (input.moveVector.x != 0)
+                {
+                    animator.Play("Run");
+                }
+                else
+                {
+                    animator.Play("Idle");
+                }
             }
             else
             {
-                animator.Play("Idle");
+                if (rb2d.velocity.y > 0)
+                {
+                    animator.Play("Jump");
+                }
+                else
+                {
+                    animator.Play("fall");
+                }
             }
-        }
-        else
-        {
-            if (rb2d.velocity.y > 0)
+
+            if (expr)
             {
-                animator.Play("Jump");
-            }
-            else
-            {
-                animator.Play("fall");
+                
             }
         }
 
         if (input.attack)
         {
+            action = "attack";
             animator.Play("Attack");
         }
         
+    }
+
+    private void SetAction(string str)
+    {
+        action = str;
     }
 }
